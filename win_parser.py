b1d8227e3c5840a1e28fa283743cf891a3745031
Ashu Saini <sainiaashu31@gmail.com>
2026-09-12 16:17:56 +0530

    Added real Windows log parsing logic

---
+++ win_parser.py
@@ -0,0 +1,31 @@
+#Open and read the raw windows Security event log
+with open("win_log.txt", "r", encoding="utf-8") as file:
+    lines = file.readlines()
+    
+print ("===WINDOWS LOGON AUDIT REPROT ===")
+failed_count = 0
+success_count = 0
+
+for line in lines:
+    # Winoes event log fields are seprated by tab characters(\t)
+    columns = line.strip().split("\t")
+    
+    # Ensure the line has enough columns to avoid errors
+    if len(columns) > 3:
+        event_id = columns[3].strip() # The Event ID column
+        
+        # Event 4625 = Failed Logon Attempt
+        if event_id == "4625":
+            failed_count += 1
+            print(f"[FAILED LOGON] Event detected at: {columns[1]}")
+            
+        # Event 4624 = Successful Logon Attempt
+        elif event_id == "4624":
+            success_count += 1
+             
+print("\n=== SYSTEM OVERVIEW ===")
+print(f"Total Successful Logins: {success_count}")
+print(f"Total Failed Logins: {failed_count}")
+
+if failed_count > 5:
+    print("[ ALERT ] High volume of failed logons! Check for active brute-force threat.")
